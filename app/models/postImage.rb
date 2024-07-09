7e14dac098f25679cd484c2a3463d7f9d43edfe3
EC2 Default User <ec2-user@ip-172-31-4-116.ap-northeast-1.compute.internal>
2024-07-09 14:51:56 +0000

    [finish]4-20

--- app/models/postImage.rb
+++ app/models/postImage.rb
@@ -3,6 +3,7 @@ class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   def get_image
     unless image.attached?
@@ -12,4 +13,7 @@ class PostImage < ApplicationRecord
     image
   end
 
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show, :edit, :update]
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resource :favorite, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
